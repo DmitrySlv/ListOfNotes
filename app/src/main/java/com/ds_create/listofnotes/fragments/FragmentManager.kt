74e91bd1340f0ee6f7552660c911aec9078a8e17
DmitrySlv <webmaestros@yandex.ru>
2022-11-08 20:54:40 +0400

    Создание FragmentManager.kt и BaseFragment.kt классов.

---
+++ app/src/main/java/com/ds_create/listofnotes/fragments/FragmentManager.kt
@@ -0,0 +1,15 @@
+package com.ds_create.listofnotes.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import com.ds_create.listofnotes.R
+
+object FragmentManager {
+    var currentFrag: BaseFragment? = null
+
+    fun setFragment(newFrag: BaseFragment, activity: AppCompatActivity) {
+        val transaction = activity.supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, newFrag)
+        transaction.commit()
+        currentFrag = newFrag
+    }
+}
