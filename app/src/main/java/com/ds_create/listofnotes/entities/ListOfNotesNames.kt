732a0d893738178da4fe347f9b819d2d88bcf0be
DmitrySlv <webmaestros@yandex.ru>
2022-11-08 16:57:44 +0400

    Создание Entities. Добавление data class ListOfNotesNames.kt в котором создаются entities для заполнения в таблице.

---
+++ app/src/main/java/com/ds_create/listofnotes/entities/ListOfNotesNames.kt
@@ -0,0 +1,29 @@
+package com.ds_create.listofnotes.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity (tableName = "list_of_notes_names")
+data class ListOfNotesNames(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int?,
+
+    @ColumnInfo (name = "name")
+    val name: String,
+
+    @ColumnInfo (name = "time")
+    val time: String,
+
+    @ColumnInfo (name = "allItemCounter")
+    val allItemCounter: Int,
+
+    @ColumnInfo (name = "checkedItemsCounter")
+    val checkedItemsCounter: Int,
+
+    @ColumnInfo (name = "itemsIds")
+    val itemsIds: String
+
+): Serializable
