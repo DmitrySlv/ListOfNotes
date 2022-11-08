f21dc6244db49f7b207123586b7552d1f90bd28a
DmitrySlv <webmaestros@yandex.ru>
2022-11-08 20:43:41 +0400

    Создание нижнего меню BottomNavigationView. Создание bottom_menu.xml и добавление иконок к меню. Добавление цвета для actionBar и удаление statusBar. Подключение viewBinding и инициализация bottom_menu.xml в MainActivity.kt

--- app/src/main/java/com/ds_create/listofnotes/activities/MainActivity.kt
+++ app/src/main/java/com/ds_create/listofnotes/activities/MainActivity.kt
@@ -2,11 +2,37 @@ package com.ds_create.listofnotes.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.ds_create.listofnotes.R
+import com.ds_create.listofnotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setBottomNavListener()
+    }
+
+    private fun setBottomNavListener() {
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Log.d("MyLog", "Settings")
+                }
+                R.id.notes -> {
+                    Log.d("MyLog", "Notes")
+                }
+                R.id.list_of_notes -> {
+                    Log.d("MyLog", "List of notes")
+                }
+                R.id.new_item -> {
+                    Log.d("MyLog", "New")
+                }
+            }
+            true
+        }
     }
 }
