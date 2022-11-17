5379dcdf4d0742368ae9e3837892cc2657fb9c1a
DmitrySlv <webmaestros@yandex.ru>
2022-11-17 19:59:19 +0400

    Создание ShopListActivity ч2. Создание list_menu.xml для ListActivity.kt и создание выплывающего меню по нажатию на три точки с помощью action_layout.xml По нажатию на кнопку из меню "добавить заметку" происходит разворачивание новой actionView. Инициализация меню list_menu.xml в ListActivity.kt

--- app/src/main/java/com/ds_create/listofnotes/activities/ListActivity.kt
+++ app/src/main/java/com/ds_create/listofnotes/activities/ListActivity.kt
@@ -1,8 +1,10 @@
 package com.ds_create.listofnotes.activities
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.ds_create.listofnotes.R
 import com.ds_create.listofnotes.databinding.ActivityListBinding
 import com.ds_create.listofnotes.entities.ListOfNotesNameItem
 import com.ds_create.listofnotes.viewModels.MainViewModel
@@ -25,6 +27,11 @@ class ListActivity : AppCompatActivity() {
     private fun init() = with(binding) {
         listNameItem = intent.getSerializableExtra(LIST_NAME) as ListOfNotesNameItem
         tvTest.text = listNameItem?.name
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.list_menu, menu)
+        return true
     }
 
     companion object {
