afc4ef81b351e5294c33b5121d6e66f3c9269796
DmitrySlv <webmaestros@yandex.ru>
2022-11-17 20:59:51 +0400

    Создание ShopListActivity ч3. Редактирование в list_menu.xml поля android:actionLayout="@layout/edit_action_layout" с андройд на app: и рефактор названия разметки action_layout.xml на edit_action_layout.xml. В ListActivity.kt создание глобальной переменной private lateinit var saveItem: MenuItem и инициализация её в override fun onCreateOptionsMenu(). Создание private fun expandActionView() - которая следит за нажатием и открытием edit_action_layout.xml для того чтобы была возможность показать кнопку saveItem при вводе текста, а после сохранения текста прятать saveItem. С помощью invalidateOptionsMenu() - перезапуск перерисовки меню после сохранения, для корректного отображения кнопок. Инициализация кнопки newItem и добавление к ней слешателя .setOnActionExpandListener(expandActionView()) в override fun onCreateOptionsMenu(menu: Menu?).

--- app/src/main/java/com/ds_create/listofnotes/activities/ListActivity.kt
+++ app/src/main/java/com/ds_create/listofnotes/activities/ListActivity.kt
@@ -2,6 +2,8 @@ package com.ds_create.listofnotes.activities
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.view.MenuItem.OnActionExpandListener
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.ds_create.listofnotes.R
@@ -17,6 +19,7 @@ class ListActivity : AppCompatActivity() {
         MainViewModelFactory((applicationContext as MainApp).database)
     }
     private var listNameItem: ListOfNotesNameItem? = null
+    private lateinit var saveItem: MenuItem
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +34,27 @@ class ListActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.list_menu, menu)
+        saveItem = menu?.findItem(R.id.save_item)!!
+        val newItem = menu.findItem(R.id.new_item)
+        newItem.setOnActionExpandListener(expandActionView())
+        saveItem.isVisible = false
         return true
+    }
+
+    private fun expandActionView(): OnActionExpandListener {
+        return object : OnActionExpandListener {
+
+            override fun onMenuItemActionExpand(p0: MenuItem?): Boolean {
+                saveItem.isVisible = true
+                return true
+            }
+
+            override fun onMenuItemActionCollapse(p0: MenuItem?): Boolean {
+                saveItem.isVisible = false
+                invalidateOptionsMenu()
+                return true
+            }
+        }
     }
 
     companion object {
